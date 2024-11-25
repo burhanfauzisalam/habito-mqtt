4ea87000328ebfa21ea982ce96b9449ceffbb190
burhanfauzisalam <burhanfauzisalam@gmail.com>
2024-11-25 20:31:39 +0700

    ...

--- usersDB.js
+++ usersDB.js
@@ -3,7 +3,7 @@ const { queryDatabase } = require("./db");
 const createUsersTable = async() => {
   const sql = `
     CREATE TABLE IF NOT EXISTS users (
-      id int PRIMARY KEY,
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
       username varchar(50) NOT NULL,
       password varchar(255) NOT NULL,
       created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP
